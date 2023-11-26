81c01f4746cebabf05e65661071048cac205ae1a
J. Jesús González Mairena <jiuxej@gmail.com>
2023-11-26 20:20:50 +0100

    Fixed agarre bug

--- AreaCabeza.gd
+++ AreaCabeza.gd
@@ -8,4 +8,8 @@ func _on_Area2D_body_entered(body: PhysicsBody2D):
 
 func _on_Area2D_body_exited(body: PhysicsBody2D):
 	if body != null and body.get_instance_id() in bodies:
+		if body.is_agarrado:
+			body.brazo.soltar_agarre()
+			body.is_agarrado = false
+			body.brazo.is_agarrando = false 
 		var _manita_fuera = bodies.erase(body.get_instance_id())
