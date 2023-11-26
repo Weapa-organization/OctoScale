4203a69a76777ae3b5d3bb4ca9fa8cbfb745fc2a
Antonio Godoy Gonzalez <43647734+AntonioGodoyUGR@users.noreply.github.com>
2023-11-26 15:46:35 +0100

    pulpo

--- scripts/script_mano.gd
+++ scripts/script_mano.gd
@@ -6,7 +6,7 @@ var is_agarrando : bool = false
 
 func coger_agarre(posicion_agarre: Vector2):
 	self.mode = MODE_STATIC
-	$Tween.targeting_property(self, "global_position", self, "global_position", posicion_agarre, 2, Tween.TRANS_CIRC, Tween.EASE_IN_OUT)
+	$Tween.targeting_property(self, "global_position", self, "global_position", posicion_agarre, 1, Tween.TRANS_CIRC, Tween.EASE_IN_OUT)
 	$Tween.start()
 
 func soltar_agarre():
