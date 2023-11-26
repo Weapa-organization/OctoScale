a61dca0fa32454fb58d35544c69b50f9c2fdcbf5
J. Jesús González Mairena <jiuxej@gmail.com>
2023-11-26 12:30:31 +0100

    Fix Camara

--- scripts/move_camera.gd
+++ scripts/move_camera.gd
@@ -13,5 +13,5 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	global_position = get_parent().get_node("Cabeza").global_position
+	global_position = get_parent().get_node("Cabeza").global_position + Vector2(0, -230)
 	pass
